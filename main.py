b218bc4857c4e1a656401de5f433acceade29dae
gregreust <greg.reust@gmail.com>
2022-07-25 16:38:33 -0500

    Ready for debugging

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ from game import Game
 #create game
 #run game
 #ask if player wants to play again 
+
+game = Game()
+game.run_game()
+user_input = ''
+while user_input != 'y' and user_input != 'n':
+    user_input = input('Do you want to play again? Enter y or n')
+    if user_input == 'y':
+        game.run_game()
+    elif user_input == 'n':
+        break
+    else:
+        print('Error')
